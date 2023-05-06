65086f94583a096b205c7f77539f06cab43a74b8
Jorge Garcia <jegcelorio@yahoo.com>
2023-05-05 20:15:18 -0500

    Added MainDisplay component

---
+++ src/components/mainDisplay.js
@@ -0,0 +1,17 @@
+export default function MainDisplay() {
+    return(
+        <article className="main-display">
+            <section className="action-section">
+                <section className="copy-section">
+                    <p>Inside copy section</p>
+                </section>
+                <section className="form-section">
+                    <p>Form section</p>
+                </section>
+            </section>
+            <section className="image-section">
+                <p>Image</p>
+            </section>
+        </article>
+    );
+}
