86fc991d3558bbba4d39250c3ab63c85ea3d23e7
Jorge Garcia <jegcelorio@yahoo.com>
2023-05-05 20:17:40 -0500

    Imported Footer component

--- src/pages/landing_page.js
+++ src/pages/landing_page.js
@@ -1,11 +1,13 @@
 import NavBar from "@/components/navbar";
 import MainDisplay from "@/components/mainDisplay";
+import Footer from "@/components/footer";
 
 export default function LandingPage() {
     return(
         <>
             <NavBar />
             <MainDisplay />
+            <Footer />
         </>
     );
 }
