0a28b6f26b590edbc947a92a8f6ec6d4bb56bedd
Jorge Garcia <jegcelorio@yahoo.com>
2023-05-06 23:43:09 -0500

    Changed attribute to className

--- src/pages/landing_page.js
+++ src/pages/landing_page.js
@@ -6,7 +6,7 @@ export default function LandingPage() {
     return(
         <>
             <NavBar />
-            <h1 class="text-3xl font-bold underline">Let's test tailwindcss</h1>
+            <h1 className="text-3xl font-bold underline">Let's test tailwindcss</h1>
             <MainDisplay />
             <Footer />
         </>
