3b2e5e9ef252731eab6ecd0072040bf623b9bc68
Jorge Garcia <jegcelorio@yahoo.com>
2023-05-06 20:11:02 -0500

    Added file paths to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    // using `src` directory
+    "./src/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
   theme: {
     extend: {},
   },
