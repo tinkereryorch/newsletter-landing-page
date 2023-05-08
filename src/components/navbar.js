b932c68337caad9eea1be1b5e7e6db44b8c651ea
Jorge Garcia <jegcelorio@yahoo.com>
2023-05-08 00:02:15 -0500

    Styled navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,14 @@
 export default function NavBar() {
     return(
-        <nav>
-            <div className="nav-container">
-                <div>
-                    <p>Floral tips</p>
+        <nav className="bg-floral-gray font-sans text-4xl">
+            <section className="flex items-center justify-between px-12 py-4">
+                <div className="text-white logo">
+                    <p>Floral Shop News</p>
                 </div>
-                <div>
-                    <p>Profile logo</p>
+                <div className="w-12 h-12 rounded-full  bg-floral-purple flex justify-center items-center">
+                    <span className="text-white text-3xl">F</span>
                 </div>
-            </div>
+            </section>
         </nav>
     );
 }
