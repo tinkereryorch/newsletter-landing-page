87a3afbd6004065748436881e172cf75639192f9
Jorge Garcia <jegcelorio@yahoo.com>
2023-05-08 00:09:08 -0500

    linted file

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,7 +5,7 @@ export default function NavBar() {
                 <div className="text-white logo">
                     <p>Floral Shop News</p>
                 </div>
-                <div className="w-12 h-12 rounded-full  bg-floral-purple flex justify-center items-center">
+                <div className="w-12 h-12 rounded-full bg-floral-purple flex justify-center items-center">
                     <span className="text-white text-3xl">F</span>
                 </div>
             </section>
