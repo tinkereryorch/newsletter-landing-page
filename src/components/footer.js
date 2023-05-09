7c9dd725e9a196e6eeef77be61dc15d0de9af23e
Jorge Garcia <jegcelorio@yahoo.com>
2023-05-08 22:47:28 -0500

    Styled footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
     return(
         <footer className="fixed bottom-0 w-full">
             <section className="bg-floral-fill">
-                <p className="">Footer message</p>
+                <p className="p-4 text-center text-xl text-info-text">© Floral Shop News 2023</p>
             </section>
         </footer>
     );
