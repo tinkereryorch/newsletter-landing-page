7ad305e9e48e6018359562494aac1666a6305544
Jorge Garcia <jegcelorio@yahoo.com>
2023-05-08 22:47:45 -0500

    Styled input and button

--- src/components/mainDisplay.js
+++ src/components/mainDisplay.js
@@ -4,10 +4,13 @@ export default function MainDisplay() {
             <section className="action-section pt-60">
                 <section className="copy-section px-10">
                     <p className="text-7xl leading-snug">Imagine setting up your own floral shop.</p>
+                    <p className="text-4xl px-2 py-2">Join our newsletter</p>
                 </section>
-                <section className="form-section px-10">
-                    <input placeholder="Here goes the input" />
-                    <button>Join</button>
+                <section className="flex form-section px-10">
+                    <section className="bg-floral-purple p-2 rounded-md w-4/12">
+                        <input className="rounded-md p-1 w-full"placeholder="your email" />
+                    </section>
+                    <button className="ml-4 rounded-md p-1 w-32 text-white text-xl bg-floral-olive">Join</button>
                 </section>
             </section>
             <figure className="figure-section py-20 pr-20">
