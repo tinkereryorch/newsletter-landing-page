2312577a4ee21c980b2a51712ba7a27538cbbe74
Jorge Garcia <jegcelorio@yahoo.com>
2023-05-08 19:11:27 -0500

    Added initial styles to mainDisplay

--- src/components/mainDisplay.js
+++ src/components/mainDisplay.js
@@ -1,15 +1,16 @@
 export default function MainDisplay() {
     return(
-        <article className="main-display">
-            <section className="action-section">
-                <section className="copy-section">
+        <article className="main-display bg-floral-fill px-10 py-10">
+            <section className="action-section px-10 py-10">
+                <section className="copy-section px-10 py-10">
                     <p>Inside copy section</p>
                 </section>
-                <section className="form-section">
-                    <p>Form section</p>
+                <section className="form-section px-10 py-10">
+                    <input placeholder="Here goes the input" />
+                    <button>Join</button>
                 </section>
             </section>
-            <figure className="figure-section">
+            <figure className="figure-section px-10 py-10">
                 <p>Image</p>
             </figure>
         </article>
